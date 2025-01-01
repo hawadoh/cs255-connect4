43b973c4aa01a3a04764b7bbb3dd089e19601a9b
Howard Cheung <112148135+hawadoh@users.noreply.github.com>
2025-01-01 23:41:30 +0800

    Created connect4.py

---
+++ connect4.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+def create_board():
+    return np.zeros((ROW_COUNT,COLUMN_COUNT))
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def is_valid_location(board, col):
+    return board[5][col] == 0
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+        
+def print_board(board):
+    print(np.flip(board, 0))
+
+def winning_move(board, piece):
+    pass
+
+board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+    # Ask for Player 1 Input
+    if turn == 0:
+        selection = int(input("Player 1 Make your selection (0-6): "))
+        if is_valid_location(board, selection):
+            row = get_next_open_row(board, selection)
+            drop_piece(board, row, selection, 1)
+            print_board(board)
+            turn += 1
+            turn = turn % 2
+        else:
+            print("Invalid Selection. Try Again.")
+    # Ask for Player 2 Input
+    else:
+        selection = int(input("Player 2 Make your selection (0-6): "))
+        if is_valid_location(board, selection):
+            row = get_next_open_row(board, selection)
+            drop_piece(board, row, selection, 2)
+            print_board(board)
+            turn += 1
+            turn = turn % 2
+        else:
+            print("Invalid Selection. Try Again.")
